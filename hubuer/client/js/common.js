63538325dc43757534b554feb61c26425c53e144
Your Name <you@example.com>
2018-03-31 20:13:41 +0800

    add index.html

---
+++ hubuer/client/js/common.js
@@ -0,0 +1,5 @@
+//$("#test").append("我是测试的内容！更新啦")
+var func = str => {
+     document.getElementById('test').innerHTML = str;
+ };
+// func('我现在在使用Babel!命令简化成功');
---
+++ hubuer/client/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+ 
+    /*入口*/
+   entry: [
+//  	'react-hot-loader/patch',
+    	path.join(__dirname, 'js/common.js')
+    ],
+    
+    /*输出到dist文件夹，输出文件名字为bundle.js*/
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    
+    /*src文件夹下面的以.js结尾的文件，要使用babel解析*/
+	/*cacheDirectory是用来缓存编译结果，下次编译加速*/
+	module: {
+	    rules: [
+		    {
+		        test: /\.js$/,
+		        use: ['babel-loader?cacheDirectory=true'],
+		        include: path.join(__dirname, 'src')
+		    },
+		    {
+		        test: /\.css$/,
+		        use: [
+		          { loader: 'style-loader' },
+		          {
+		            loader: 'css-loader',
+		            options: {modules: true}
+		          }
+		        ]
+		    }
+	    ]
+	},
+	 
+	devServer: {
+        contentBase: path.join(__dirname, './dist')
+    }
+};
